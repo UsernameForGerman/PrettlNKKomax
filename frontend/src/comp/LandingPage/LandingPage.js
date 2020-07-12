c92392345612aab8e9156e1f9df9c7b9cece0e24
Whatruska <vitdub@inbox.ru>
2020-07-13 01:19:54 +0600

    [Landing page] Graphs and stages are resonsive

--- frontend/src/comp/LandingPage/LandingPage.js
+++ frontend/src/comp/LandingPage/LandingPage.js
@@ -3,12 +3,14 @@ import classes from "./LandingPage.module.css";
 import LandingHeading from "./LandingHeading/LandingHeading";
 import DescSection from "./DescSection/DescSection";
 import Infographics from "./Infographics/Infographics";
+import Stages from "./Stages/Stages";
 let LandingPage = (props) => {
     return (
         <div className={classes.container}>
             <LandingHeading/>
             <DescSection/>
             <Infographics/>
+            <Stages/>
         </div>
     )
 }
