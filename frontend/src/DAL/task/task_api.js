287f3dbd2978ccedf8720f8c191a6e89efa1d085
Whatruska <vitdub@inbox.ru>
2020-07-14 23:34:21 +0600

    [Tasks] Task stop + delete fix

--- frontend/src/DAL/task/task_api.js
+++ frontend/src/DAL/task/task_api.js
@@ -32,7 +32,7 @@ class task_api extends API{
     }
 
     deleteTask = (task) => {
-        return this.createAPI().delete(BASE_URL + task.name + "", {task_name : task.name});
+        return this.createAPI().delete(BASE_URL + task.task_name + "", {task_name : task.task_name});
     }
 }
 
