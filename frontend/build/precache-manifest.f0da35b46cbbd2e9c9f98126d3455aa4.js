3f39f795b9ef79cf691804a63a88d7976b8dbeb9
German <polyakgerman@gmail.com>
2020-07-27 04:49:20 +0300

    [Front] - create build and node_modules

---
+++ frontend/build/precache-manifest.f0da35b46cbbd2e9c9f98126d3455aa4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b111c9afe25a4b1fc6d2c7ec6bc3111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05ca6c8cd87750a5fdb3",
+    "url": "/static/css/2.fb3de481.chunk.css"
+  },
+  {
+    "revision": "092263758291bafa60eb",
+    "url": "/static/css/main.b949c5d3.chunk.css"
+  },
+  {
+    "revision": "05ca6c8cd87750a5fdb3",
+    "url": "/static/js/2.be105216.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/static/js/2.be105216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092263758291bafa60eb",
+    "url": "/static/js/main.3ce0b3cc.chunk.js"
+  },
+  {
+    "revision": "1c6ea3e5aec3a5f1ff88",
+    "url": "/static/js/runtime-main.5089e9b6.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/static/media/Preloader.26ef8bb4.gif"
+  },
+  {
+    "revision": "63ead04220c2f1062bb282d3a7ae6f54",
+    "url": "/static/media/ava.63ead042.png"
+  },
+  {
+    "revision": "81d6c007f1083431f3f39325f1d73c99",
+    "url": "/static/media/komax.81d6c007.png"
+  },
+  {
+    "revision": "de62e04bd3dd790adfb8d6ec97b19fe0",
+    "url": "/static/media/stages.de62e04b.png"
+  }
+]);
